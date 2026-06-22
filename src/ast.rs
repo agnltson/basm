8b1b3e5d27af84490c43044ff7857b3cb7549e8b
Antoine Glasson <ant.glasson@gmail.com>
2026-06-22 20:12:01 +0200

    fix: remove programm size in metadata

--- src/ast.rs
+++ src/ast.rs
@@ -17,7 +17,6 @@ impl Program {
 impl Into<Vec<u8>> for Program {
     fn into(self) -> Vec<u8> {
         let magic: Vec<u8> = 0xD12EA2E2u32.to_le_bytes().to_vec();
-        let size: Vec<u8> = (self.instructions.len() as u32).to_le_bytes().to_vec();
 
         let instructions: Vec<u8> = self.instructions
             .into_iter()
@@ -27,7 +26,7 @@ impl Into<Vec<u8>> for Program {
             })
             .collect();
 
-        [magic, size, instructions].concat()
+        [magic, instructions].concat()
     }
 }
 
